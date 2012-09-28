eca7c1dd9ddc5f47a3266b345ff5e9efb1a9bc43
mifth <mifth@localhost>
2012-09-28 13:28:47 +0400

    LightBlow - lightmaps SRGBToLinear fix.

---
+++ src/com/fakeparticleblow/FakeParticleBlow.java
@@ -0,0 +1,69 @@
+
+
+package com.fakeparticleblow;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.asset.TextureKey;
+import com.jme3.material.Material;
+import com.jme3.renderer.queue.RenderQueue.Bucket;
+import com.jme3.scene.Spatial;
+import com.jme3.texture.Texture;
+import com.jme3.util.SkyFactory;
+
+/** Sample 6 - how to give an object's surface a material and texture.
+ * How to make objects transparent, or let colors "leak" through partially
+ * transparent textures. How to make bumpy and shiny surfaces.  */
+public class FakeParticleBlow extends SimpleApplication {
+
+    
+
+    
+  public static void main(String[] args) {
+    FakeParticleBlow app = new FakeParticleBlow();
+    app.start();
+  }
+
+    
+    
+  @Override
+  public void simpleInitApp() {
+
+      
+      TextureKey skylow = new TextureKey("Textures/Water32.dds", true);
+        skylow.setGenerateMips(true);
+        skylow.setAsCube(true);
+      final  Texture texlow = assetManager.loadTexture(skylow);
+   
+       rootNode.attachChild(SkyFactory.createSky(assetManager, texlow, false));
+
+       
+      Spatial fire = assetManager.loadModel("Models/FakeParticleBlow/FakeParticleBlow.j3o");
+       Material mat = assetManager.loadMaterial("Materials/FakeParticleBlow/FakeParticleBlow.j3m");
+       fire.setMaterial(mat);
+       fire.setQueueBucket(Bucket.Transparent); 
+      rootNode.attachChild(fire);
+
+       Spatial fire2 = assetManager.loadModel("Models/FakeParticleBlow/FakeParticleBlow.j3o");
+       Material mat2 = assetManager.loadMaterial("Materials/FakeParticleBlow/FakeParticleBlow_2.j3m");
+       fire2.setMaterial(mat2);
+       fire2.setQueueBucket(Bucket.Transparent); 
+       fire2.setLocalTranslation(2, 0, 0);
+       rootNode.attachChild(fire2);      
+       
+
+
+      flyCam.setMoveSpeed(5);   
+  
+  }
+  
+
+@Override
+    public void simpleUpdate(float tpf){
+ 
+
+    }
+    
+    
+    }
+  
+
---
+++ src/com/simplesprite/SimpleSprite.java
@@ -0,0 +1,70 @@
+package com.simplesprite;
+
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Spatial;
+import com.jme3.util.TangentBinormalGenerator;
+
+
+public class SimpleSprite extends SimpleApplication {
+
+    
+  public static void main(String[] args) {
+    SimpleSprite app = new SimpleSprite();
+    app.start();
+  }
+
+  
+    
+    
+  @Override
+  public void simpleInitApp() {
+
+    Spatial char_boy1 = assetManager.loadModel("Models/SimpleSprite/SimpleSprite.blend");
+    Material mat1 = assetManager.loadMaterial("Materials/SimpleSprite/SimpleSprite_1.j3m");
+    char_boy1.setMaterial(mat1);
+    char_boy1.setLocalTranslation(0,0,0);
+    TangentBinormalGenerator.generate(char_boy1);
+    rootNode.attachChild(char_boy1);
+
+    Spatial char_boy2 = assetManager.loadModel("Models/SimpleSprite/SimpleSprite.blend");
+    Material mat2 = assetManager.loadMaterial("Materials/SimpleSprite/SimpleSprite_2.j3m");
+    char_boy2.setMaterial(mat2);
+    char_boy2.setLocalTranslation(1,0,0);
+    char_boy2.setLocalScale(0.5f, 1, 1);    
+    TangentBinormalGenerator.generate(char_boy2);
+    rootNode.attachChild(char_boy2);    
+
+//        Spatial char_boy3 = assetManager.loadModel("Models/LightBlow/jme_lightblow.obj");
+//    Material mat3 = assetManager.loadMaterial("Materials/MatCap/MatCap2.j3m");
+//    char_boy3.setMaterial(mat3);
+//    char_boy3.setLocalTranslation(-1,0,0);
+//    TangentBinormalGenerator.generate(char_boy3);
+//    rootNode.attachChild(char_boy3);
+//
+//    Spatial char_boy4 = assetManager.loadModel("Models/LightBlow/jme_lightblow.obj");
+//    Material mat4 = assetManager.loadMaterial("Materials/MatCap/MatCapBump2.j3m");
+//    char_boy4.setMaterial(mat4);
+//    char_boy4.setLocalTranslation(-2,0,0);
+//    TangentBinormalGenerator.generate(char_boy4);
+//    rootNode.attachChild(char_boy4);    
+
+
+      flyCam.setMoveSpeed(10);   
+      viewPort.setBackgroundColor(ColorRGBA.Gray);      
+  
+  }
+  
+
+@Override
+    public void simpleUpdate(float tpf){
+ 
+
+    }
+    
+    
+    }
+  
+
